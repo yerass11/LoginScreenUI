77d05b1aac8dafcbb6764ad4971882bc93e81514
Yerassyl <Erasylsaiman2005@gmail.com>
2023-04-26 14:20:20 +0600

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Erasyl on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
